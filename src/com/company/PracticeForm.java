94e34c5e9f7d6a1f94ff8a063ce96c65eb45baf4
1kvictoria <victoria.akhchiyani@hotmail.com>
2015-01-21 20:58:58 +0200

    2 commit

--- src/com/company/PracticeForm.java
+++ src/com/company/PracticeForm.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  * Created by user on 1/21/15.
  */
 public class PracticeForm {
+    public static void main (String[] args) throws InterruptedException {
+        int n = 0;
+        String[] input = {"синий", "белый", "черный", "желтый"};
+        String[] output = {"blue", "white", "black", "yellow"};
+        System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
+
+        WebDriver a = new ChromeDriver();
+        a.get("http://www.toolsqa.com/automation-practice-form");
+        WebElement i = a.findElement(By.id("sex-1"));
+
+    }
 }
