3d27356a2a0ec5b0aaa0a65ea13b968f49603f61
1kvictoria <victoria.akhchiyani@hotmail.com>
2015-01-23 20:42:26 +0200

    2 commit

--- src/com/company/PracticeForm.java
+++ src/com/company/PracticeForm.java
@@ -1,9 +1,11 @@
 package com.company;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by user on 1/21/15.
@@ -13,16 +15,54 @@ public class PracticeForm {
         int n = 0;
         //String[] input = {"синий", "белый", "черный", "желтый"};
        // String[] output = {"blue", "white", "black", "yellow"};
-        //System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", "D:/Soft/IDE/chromedriver_win32/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "D:/Soft/IDE/chromedriver_win32/chromedriver.exe");
         //Automation Practice Form
         WebDriver a = new ChromeDriver();
         a.get("http://www.toolsqa.com/automation-practice-form");
         Thread.sleep(3000);
-       // WebElement link = a.findElement(By.tagName("a"));
-        //link.click();
+        WebElement link1 = a.findElement(By.id("content")).findElements(By.tagName("a")).get(0);
+        //link1.click();
 
-        WebElement name = a.findElement(By.name("firstname"));
+        if (link1.getAttribute("href").equals("http://www.toolsqa.com/automation-practice-form/")){
+            System.out.println("Test link1  : passed ");
+        } else {
+            System.out.println("Test link1  : failed ");
+            System.out.println(link1.getAttribute("href"));
+        }
+        WebElement cont = a.findElement(By.id("continents"));
+        Select s = new Select(cont);
+        s.selectByVisibleText("Europe");
+        System.out.println(s.getFirstSelectedOption().getText());
+
+        WebElement command = a.findElement(By.id("selenium_commands"));
+        s = new Select(command);
+       // s.selectByVisibleText("Navigation Commands"); //Switch Commands
+        s.selectByIndex(1);
+        command.sendKeys(Keys.CONTROL);
+        s.selectByIndex(2);
+
+        //command.sendKeys(Keys.SHIFT);
+        //s.selectByIndex('4');
+        n=0;
+        while (n < s.getAllSelectedOptions().size()) {
+            System.out.println(s.getAllSelectedOptions().get(n).getText());
+            n++;
+        }
+
+
+       // setClipboardData("C:\\path to file\\example.jpg");
+//native key strokes for CTRL, V and ENTER keys
+      //  Robot robot = new Robot();
+     //   robot.keyPress(KeyEvent.VK_CONTROL);
+      //  robot.keyPress(KeyEvent.VK_V);
+      //  robot.keyRelease(KeyEvent.VK_V);
+      //  robot.keyRelease(KeyEvent.VK_CONTROL);
+      //  robot.keyPress(KeyEvent.VK_ENTER);
+     //   robot.keyRelease(KeyEvent.VK_ENTER);
+
+     //   http://demoqa.com/
+      /*  WebElement name = a.findElement(By.name("firstname"));
         name.sendKeys("Testa");
         name = a.findElement(By.name("lastname"));
         name.sendKeys("Testik");
@@ -50,5 +90,7 @@ public class PracticeForm {
         prof.click();//prof.isSelected();
         WebElement photo = a.findElement(By.id("photo"));
         photo.click(); //kot_40912552_big_.jpeg
+        */
+        a.quit();
     }
 }
