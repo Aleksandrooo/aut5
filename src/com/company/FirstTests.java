d53e4323352b654b9d4eba14a191b5c340710cd6
Aleksandrooo <trofimenko.alex.v@gmail.com>
2015-01-30 00:35:34 +0200

    commit

--- src/com/company/FirstTests.java
+++ src/com/company/FirstTests.java
@@ -28,64 +28,68 @@ public class FirstTests {
 
     @Before
     public void  Init() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver", "D:/Soft/IDE/chromedriver_win32/chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:/Soft/IDE/chromedriver_win32/chromedriver.exe");
         w = new ChromeDriver();
-        w.get("http://demoqa.com/registration/");
+        //w.get("http://demoqa.com/registration/");
+        RegistrationForm.open(w);
+        testPassed = false;
         Thread.sleep(3000);
     }
 
     @After
     public void  CleanUp() throws IOException {
-        File scrFile = ((TakesScreenshot)w).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(scrFile, new File("c:\\tmp\\" + name.getMethodName() + ".png"));
-                w.quit();
+        if (!testPassed) {
+            File scrFile = ((TakesScreenshot)w).getScreenshotAs(OutputType.FILE);
+            //FileUtils.copyFile(scrFile, new File("c:\\tmp\\" + name.getMethodName() + ".png"));
+            FileUtils.copyFile(scrFile, new File("d:\\Testing\\temp\\" + name.getMethodName() + ".png"));
+        }
+        w.quit();
     }
 
     @Test
-    public void MyTest() throws InterruptedException, IOException {
-       //int a= 6,  b =7;
-       // Assert.assertEquals(a, b);
-        Init();
-        WebElement Name = w.findElement(By.id("name_3_firstname"));
-        // Name = a.findElement(By.xpath("//input[@name='first_name']/../label"));
-        WebElement label1 = w.findElement(By.xpath("//div[input[@name='first_name']]/label"));
-        //System.out.println("Значения  по умолчанию");
-        //printresult("Значениe по умолчанию firstname", Name.getText().equals(""));
-        //Name.sendKeys("Testa");
-        Assert.assertEquals("", Name.getText());
-        CleanUp();
+    public void MyTest1() throws InterruptedException, IOException {
+        RegistrationForm.setFirstName(w, "Testa");
+        RegistrationForm.setLastName(w, "Testik");
+        RegistrationForm.setMaritalStatus(w, 1);
+        RegistrationForm.setHobby(w, 1);
+        RegistrationForm.setCountry(w, "Ukraine");
+        RegistrationForm.setDay(w, "21");
+        RegistrationForm.setMonth(w, "10");
+        RegistrationForm.setYear(w, "2000");
+
+        Thread.sleep(3000);
+        testPassed = true;
     }
 
     @Test
     public void MyTest2() throws InterruptedException, IOException {
-        Init();
-        //WebElement Name = a.findElement(By.id("name_3_lastname"));
-        WebElement label1 = w.findElement(By.xpath("//div[input[@name='last_name']]/label"));
-        //System.out.println(label1.getText());
-        //printresult("Значение по умолчанию lastname", Name.getText().equals(""));
-        Assert.assertEquals("Last Name", label1.getText());
-        CleanUp();
+        //WebElement label1 = w.findElement(By.xpath("//div[input[@name='last_name']]/label"));
+        Assert.assertEquals("First Name", RegistrationForm.getLabelFirstName(w));
+        Assert.assertEquals("Last Name", RegistrationForm.getLabelLastName(w));
     }
 
     @Test
-    public void MyTest3() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:/AUT5/chromedriver.exe");
-        //System.setProperty("webdriver.chrome.driver", "D:/Soft/IDE/chromedriver_win32/chromedriver.exe");
-        WebDriver a = new ChromeDriver();
-        a.get("http://demoqa.com/registration/");
-        Thread.sleep(3000);
-        //WebElement Name = a.findElement(By.id("name_3_firstname"));
-        // Name = a.findElement(By.xpath("//input[@name='first_name']/../label"));
-        WebElement submit = a.findElement(By.name("pie_submit"));
-       // printresult("Значение по умолчанию submit", submit.getAttribute("value").equals("Submit"));
+    public void MyTest3() throws InterruptedException, IOException {
+        //WebElement label1 = w.findElement(By.xpath("//div[input[@name='last_name']]/label"));
+        Assert.assertEquals("", RegistrationForm.getFirstName(w));
+        Assert.assertEquals("", RegistrationForm.getLastName(w));
+        //Assert.assertEquals("", RegistrationForm.getMaritalStatus(w));
+        //Assert.assertEquals("", RegistrationForm.getHobby(w));
+        Assert.assertEquals("Month", RegistrationForm.getMonth(w));
+        Assert.assertEquals("Day", RegistrationForm.getDay(w));
+        Assert.assertEquals("Year", RegistrationForm.getYear(w));
+
+
+    }
+
+    @Test
+    public void MyTest4() throws InterruptedException {
+
+        WebElement submit = w.findElement(By.name("pie_submit"));
         submit.click();
-        WebElement errormes = a.findElement(By.xpath("//div[@class='legend_txt']/span"));
-        //System.out.println(label1.getText());
-        //System.out.println("Значения  по умолчанию");
-        //printresult("Значениe по умолчанию firstname", Name.getText().equals(""));
-        //Name.sendKeys("Testa");
+        WebElement errormes = w.findElement(By.xpath("//div[@class='legend_txt']/span"));
         Assert.assertEquals("* This field is required", errormes.getText());
-        a.quit();
+
     }
 }
--- src/com/company/RegistrationForm.java
+++ src/com/company/RegistrationForm.java
@@ -2,17 +2,139 @@ package com.company;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 /**
  * Created by user on 1/28/15.
  */
 public class RegistrationForm {
+    private static String firstNameXpath = "//input[@name='first_name']";
+    private static String lastNameXpath = "//input[@name='last_name']";
+    private static String maritalStatusXpath = "//div[@class='radio_wrap']/input[@name='radio_4[]']";
+    private static String hobbyXpath = "//div[@class='radio_wrap']/input[@name='checkbox_5[]']";
+    private static String countryXpath = "//div[@class='fieldset']/select[@name='dropdown_7']";
+    private static String monthXpath = "//select[@id='mm_date_8']";
+    private static String dayXpath = "//select[@id='dd_date_8']";
+    private static String yearXpath = "//select[@id='yy_date_8']";
+
+
+
     public static void open(WebDriver w){
        w.get("http://demoqa.com/registration/");
-
     }
 
-    public static void setFirstName(WebDriver w, String i ){
-        w.findElement((By.xpath("//div[input[@name='first_name']")));
+    public static void setFirstName(WebDriver w, String i){
+        WebElement f = w.findElement(By.xpath(firstNameXpath));
+        f.sendKeys(i);
     }
+
+    public static String getFirstName(WebDriver w){
+        WebElement f = w.findElement(By.xpath(firstNameXpath));
+        return f.getText();
+    }
+
+    public static String getLabelFirstName(WebDriver w){
+        WebElement f = w.findElement(By.xpath(firstNameXpath  + "/preceding-sibling::label"));
+        return f.getText();
+    }
+
+    public static void setLastName(WebDriver w, String i){
+        WebElement f = w.findElement(By.xpath(lastNameXpath));
+        f.sendKeys(i);
+    }
+
+    public static String getLastName(WebDriver w){
+        WebElement f = w.findElement(By.xpath(lastNameXpath));
+        return f.getText();
+    }
+
+    public static String getLabelLastName(WebDriver w){
+        WebElement f = w.findElement(By.xpath(lastNameXpath  + "/preceding-sibling::label"));
+        return f.getText();
+    }
+
+    public static void setMaritalStatus(WebDriver w, int i){
+        List<WebElement> Status = w.findElements(By.xpath(maritalStatusXpath));
+        Status.get(1).click();
+    }
+
+    public static Boolean getMaritalStatus(WebDriver w){
+        List<WebElement> Status = w.findElements(By.xpath(maritalStatusXpath));
+        int i=0; boolean bl = true;
+        while (i < Status.size()){
+            if(Status.get(i).isSelected()){bl = false; break;
+            } else{ bl = true;}
+            i++;
+        }
+        return bl;
+    }
+
+    public static void setHobby(WebDriver w, int i){
+        List<WebElement> Status = w.findElements(By.xpath(hobbyXpath));
+        Status.get(1).click();
+    }
+
+    public static Boolean getHobby(WebDriver w){
+        List<WebElement> Status = w.findElements(By.xpath(hobbyXpath));
+        int i=0; boolean bl = true;
+        while (i < Status.size()){
+            if(Status.get(i).isSelected()){bl = false; break;
+            } else{ bl = true;}
+            i++;
+        }
+        return bl;
+    }
+
+    public static void setCountry(WebDriver w, String i){
+        WebElement country = w.findElement(By.xpath(countryXpath));
+        Select s = new Select(country);
+       // printresult("Значение по умолчанию Country ",
+       //         country.findElements(By.tagName("option")).get(0).getText().equals("Afghanistan"));
+        s.selectByValue(i);
+    }
+
+    public static String getCountry(WebDriver w){
+        WebElement country = w.findElement(By.xpath(countryXpath));
+        return country.findElements(By.tagName("option")).get(0).getText();
+    }
+
+    public static void setMonth(WebDriver w, String i){
+        WebElement month = w.findElement(By.xpath(monthXpath));
+        Select s = new Select(month);
+        s.selectByValue(i);
+    }
+
+    public static String getMonth(WebDriver w){
+        WebElement month = w.findElement(By.xpath(monthXpath));
+        return month.findElements(By.tagName("option")).get(0).getText();
+    }
+
+    public static void setDay(WebDriver w, String i){
+        WebElement day = w.findElement(By.xpath(dayXpath));
+        Select s = new Select(day);
+        s.selectByValue(i);
+    }
+
+    public static String getDay(WebDriver w){
+        WebElement day = w.findElement(By.xpath(dayXpath));
+        return day.findElements(By.tagName("option")).get(0).getText();
+    }
+
+    public static void setYear(WebDriver w, String i){
+        WebElement year = w.findElement(By.xpath(yearXpath));
+        Select s = new Select(year);
+        s.selectByValue(i);
+    }
+
+    public static String getYear(WebDriver w){
+        WebElement year = w.findElement(By.xpath(yearXpath));
+        return year.findElements(By.tagName("option")).get(0).getText();
+    }
+
+
+
+
 }
